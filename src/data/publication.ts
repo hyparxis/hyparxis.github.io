6502f2bfc1a6fed60fbec311a6e112d855c97557
Pedro Morais <autranemorais@gmail.com>
2026-02-09 16:40:17 -0800

    Update papers

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -13,13 +13,24 @@ export interface Publication {
 
 export const publicationData: Publication[] = [
   {
+    year: "2025",
+    conference: "Humanoids (Sim-to-Real Workshop)",
+    title: "Towards a Motion Foundation Model: General-Purpose Whole Body Control through Zero-Shot Sim-to-Real Reinforcement Learning",
+    authors: "Jonah Siekman, Helei Duan, Pedro Morais, Chris Paxton, Izzy Brand, Yesh Godse, Alan Fern, Pras Velagapudi, Jonathan Hurst",
+    paperUrl: "https://drive.google.com/file/d/1btQvRqd5fdbNQ7s9kTakeWzctSeHnr4P/view",
+    imageUrl: "/images/humanoids2025/wbc-moving-bricks.gif",
+  },
+  {
     year: "2019",
     conference: "CoRL",
     title: "Iterative Reinforcement Learning Based Design of Dynamic Locomotion Skills for Cassie",
     authors: "Zhaoming Xie, Patrick Clary, Jeremy Dao, Pedro Morais, Jonathan Hurst, Michiel van de Panne",
     paperUrl: "https://arxiv.org/abs/1903.09537",
     imageUrl: "/images/corl2019/cassie.gif",
-    // codeUrl: "https://github.com/jsmith/robust-causal-discovery",
+    // codeUrl:
+    // bibtex:
+    // tldr:
+    // award:
   },
   {
     year: "2018",
@@ -27,12 +38,6 @@ export const publicationData: Publication[] = [
     title: "Monte-Carlo Planning for Agile Biped Locomotion",
     authors: "Patrick Clary, Pedro Morais, Alan Fern, Jonathan Hurst",
     paperUrl: "https://web.engr.oregonstate.edu/~afern/papers/icaps18-clary.pdf",
-    // codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
-    //bibtex: "https://arxiv.org/abs/2409.15476.bib",
-    // tldr: "Using causal discovery to find the causal structure of high-dimensional time series data.",
     imageUrl: "/images/mcds/pclary2018.jpg",
-    // award: "🏆 Best Paper Award",
-    // if you have an image in public/images, you can use it like this:
-    // imageUrl: "/images/publication-image.jpg"
   },
 ];
